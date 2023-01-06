42d73022e7781cc8f11811bd56ccbb04585a0761
unknown <irik.pruglo@mail.ru>
2023-01-06 08:43:33 +0100

    add ways to images

--- js/bundle.js
+++ js/bundle.js
@@ -790,84 +790,84 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "getResource": () => (/* binding */ getResource)
 /* harmony export */ });
 const books = {
-    "booksDB": [
-      {
-        "src": "https://cdn.waterstones.com/bookjackets/large/9780/2415/9780241512425.jpg",
-        "alt": "Book",
-        "author": "Richard Osman",
-        "bookName": "The Bullet That Missed",
-        "price": "15.99",
-        "descr": "It is an ordinary Thursday, and things should finally be returning to normal. Except trouble is never far away where the Thursday Murder Club are concerned. A local news legend is on the hunt for a sensational headline, and soon the gang are hot on the trail of two murders, ten years apart. To make matters worse, a new nemesis pays. Elizabeth a visit, presenting her with a deadly mission: kill or be killed…While Elizabeth grapples with her conscience (and a gun), the gang and their unlikely new friends (including TV stars, money launderers and ex-KGB colonels) unravel a new mystery.But can they catch the culprit and save Elizabeth before the murderer strikes again?",
-        "id": 1
-      },
-      {
-        "src": "https://cdn.waterstones.com/bookjackets/large/9781/3985/9781398518162.jpg",
-        "alt": "Book",
-        "author": "Colleen Hoover",
-        "bookName": "It Starts With Us",
-        "price": "14.99",
-        "descr": "it started with Atlas. Colleen Hoover tells fan favourite Atlas' side of the story and shares what comes next in this long-anticipated sequel to the #1 Sunday Times bestseller.Lily and her ex-husband, Ryle, have just settled into a civil co-parenting rhythm when she suddenly bumps into her first love, Atlas, again. After nearly two years separated, she is elated that for once, time is on their side, and she immediately says yes when Atlas asks her on a date.But her excitement is quickly hampered by the knowledge that, though they are no longer married, Ryle is still very much a part of her life - and Atlas Corrigan is the one man he will hate being in his ex-wife and daughter’s life.Switching between the perspectives of Lily and Atlas, It Starts with Us picks up right where the epilogue for the bestselling phenomenon It Ends with Us left off. Revealing more about Atlas’s past and following Lily as she embraces a second chance at true love while navigating a jealous ex-husband, it proves that 'no one delivers an emotional read like Colleen Hoover' (Anna Todd, bestselling author).",
-        "id": 2
-      },
-      {
-        "src": "https://cdn.waterstones.com/bookjackets/large/9781/8388/9781838854799.jpg",
-        "alt": "Book",
-        "author": "Alan Rickman",
-        "bookName": "Madly, Deeply: The Alan Rickman Diaries",
-        "price": 25,
-        "descr": "Alan Rickman remains one of the most beloved actors of all time across almost every genre, from his breakout role as Die Hard's villainous Hans Gruber to his heart-wrenching run as Professor Severus Snape, and beyond. His air of dignity, his sonorous voice and the knowing wit he brought to each role continue to captivate new audiences today.But Rickman's artistry wasn't confined to just his performances. Fans of memoirs at large will delight in the intimate experience of reading Rickman detailing the extraordinary and the ordinary in a way that is 'anecdotal, indiscreet, witty, gossipy and utterly candid'. He grants us access to his thoughts, not only on plays, films and the craft of acting, but also politics, friendships and life.The Rickman Diaries was written with the intention to be shared, and reading it is like listening to Rickman chatting to a close friend.",
-        "id": 3
-      },
-      {
-        "src": "https://cdn.waterstones.com/override/v2/large/9781/4449/9781444968392.jpg",
-        "alt": "Book",
-        "author": "Alice Oseman",
-        "bookName": "The Heartstopper Yearbook",
-        "price": "16",
-        "descr": "Soon to be a live-action Netflix series! Boy meets boy. Boys become friends. Boys fall in love.This joyful trip into the LGBTQ+ world of Heartstopper is the perfect gift for anyone who loves the graphic novels or Netflix TV series - from Alice Oseman, bestselling author and winner of the YA Book Prize.Now in full colour for the first time!The full-colour Heartstopper Yearbook is packed full of exclusive content from the Heartstopper universe - including never-before-seen illustrations, an exclusive mini-comic, a look back at Alice's Heartstopper artwork over the years, character profiles, trivia, and insights into her creative process - all narrated by a cartoon version of Alice herself.By the winner of the YA Book Prize, Heartstopper is about love, friendship, loyalty and mental illness. It encompasses all the small stories of Nick and Charlie's lives that together make up something larger, which speaks to all of us",
-        "id": 4
-      },
-      {
-        "src": "https://cdn.waterstones.com/override/v3/large/9781/5291/9781529149418.jpg",
-        "alt": "Book",
-        "author": "Tom Felton",
-        "bookName": "Beyond the Wand",
-        "price": "16.99",
-        "descr": "They called for a break, and Gambon magicked up a cigarette from out of his beard. He and I were often to be found outside the stage door, having 'a breath of fresh air', as we referred to it. There would be painters and plasterers and chippies and sparks, and among them all would be me and Dumbledore having a crafty cigarette.From Borrower to wizard, Tom Felton's adolescence was anything but ordinary. His early rise to fame saw him catapulted into the limelight aged just twelve when he landed the iconic role of Draco Malfoy in the Harry Potter films.Speaking with candour and his own trademark humour, Tom shares his experience of growing up on screen and as part of the wizarding world for the very first time. He tells all about his big break, what filming was really like and the lasting friendships he made during ten years as part of the franchise, as well as the highs and lows of fame and the reality of navigating adult life after filming finished.",
-        "id": 5
-      },
-      {
-        "src": "https://cdn.waterstones.com/bookjackets/large/9781/9087/9781908745903.jpg",
-        "alt": "Book",
-        "author": "Shehan Karunatilaka",
-        "bookName": "The Seven Moons of Maali Almeida",
-        "price": "16.99",
-        "descr": "A searing satire set amid the murderous mayhem of Sri Lanka beset by civil war Colombo, 1990.Maali Almeida, war photographer, gambler and closet gay, has woken up dead in what seems like a celestial visa office. His dismembered body is sinking in the serene Beira lake and he has no idea who killed him.At a time where scores are settled by death squads, suicide bombers and hired goons, the list of suspects is depressingly long, as the ghouls and ghosts with grudges who cluster round can attest. But even in the afterlife, time is running out for Maali. He has seven moons to try and contact the man and woman he loves most and lead them to a hidden cache of photos that will rock Sri Lanka.Ten years after his prizewinning novel Chinaman established him as one of Sri Lanka's foremost authors, Karunatilaka is back with a rip-roaring epic, full of mordant wit and disturbing truths.",
-        "id": 6
-      },
-      {
-        "src": "https://cdn.waterstones.com/bookjackets/large/9781/9821/9781982185824.jpg",
-        "alt": "Book",
-        "author": "Jennette McCurdy",
-        "bookName": "I'm Glad My Mom Died",
-        "price": "17.99",
-        "descr": "A heartbreaking and hilarious memoir by iCarly and Sam & Cat star Jennette McCurdy about her struggles as a former child actor-including eating disorders, addiction, and a complicated relationship with her overbearing mother-and how she retook control of her life.Jennette McCurdy was six years old when she had her first acting audition. Her mother's dream was for her only daughter to become a star, and Jennette would do anything to make her mother happy. So she went along with what Mom called Calorie restriction, eating little and weighing herself five times a day. She endured extensive at-home makeovers while Mom chided. Your eyelashes are invisible, okay? You think Dakota Fanning doesn't tint hers?' She was even showered by Mom until age sixteen while sharing her diaries, email, and all her income.",
-        "id": 7
-      },
-      {
-        "src": "https://cdn.waterstones.com/bookjackets/large/9780/2415/9780241547472.jpg",
-        "alt": "Book",
-        "author": "Greta Thunberg",
-        "bookName": "The Climate Book",
-        "price": "21.99",
-        "descr": "Greta Thunberg's speeches shook the world. With The Climate Book, she has created an essential tool for everyone who wants to help save it.It seems like an impossible task: secure a safe future for life on Earth, acting at a scale and speed that the world has never seen, in the face of vast and powerful forces--not just oil tycoons and governments, but the changing climate system itself. The odds are against us, and we are running out of time. But it doesn't have to be this way.Around the world, geophysicists and mathematicians, oceanographers and meteorologists, engineers, economists, psychologists and philosophers have been using their expertise to develop a deep understanding of the crises we face. Greta Thunberg has created The Climate Book in partnership with over one hundred of these experts in order to equip us all with this knowledge. Alongside them, Greta shares her own stories of learning, demonstrating, and uncovering greenwashing around the world, revealing the extent to which we have been kept in the dark. This is one of our biggest problems, she shows, but also our greatest source of hope. Once we are given the full picture, we will be able to act--and if a schoolchild's strike could ignite a global protest, what could we do collectively if we tried?",
-        "id": 8
-      }
-    ]
+  "booksDB": [
+    {
+      "src": "img/Bullet.jpg",
+      "alt": "Book",
+      "author": "Richard Osman",
+      "bookName": "The Bullet That Missed",
+      "price": "15.99",
+      "descr": "It is an ordinary Thursday, and things should finally be returning to normal. Except trouble is never far away where the Thursday Murder Club are concerned. A local news legend is on the hunt for a sensational headline, and soon the gang are hot on the trail of two murders, ten years apart. To make matters worse, a new nemesis pays. Elizabeth a visit, presenting her with a deadly mission: kill or be killed…While Elizabeth grapples with her conscience (and a gun), the gang and their unlikely new friends (including TV stars, money launderers and ex-KGB colonels) unravel a new mystery.But can they catch the culprit and save Elizabeth before the murderer strikes again?",
+      "id": 1
+    },
+    {
+      "src": "img/Us.jpg",
+      "alt": "Book",
+      "author": "Colleen Hoover",
+      "bookName": "It Starts With Us",
+      "price": "14.99",
+      "descr": "it started with Atlas. Colleen Hoover tells fan favourite Atlas' side of the story and shares what comes next in this long-anticipated sequel to the #1 Sunday Times bestseller.Lily and her ex-husband, Ryle, have just settled into a civil co-parenting rhythm when she suddenly bumps into her first love, Atlas, again. After nearly two years separated, she is elated that for once, time is on their side, and she immediately says yes when Atlas asks her on a date.But her excitement is quickly hampered by the knowledge that, though they are no longer married, Ryle is still very much a part of her life - and Atlas Corrigan is the one man he will hate being in his ex-wife and daughter’s life.Switching between the perspectives of Lily and Atlas, It Starts with Us picks up right where the epilogue for the bestselling phenomenon It Ends with Us left off. Revealing more about Atlas’s past and following Lily as she embraces a second chance at true love while navigating a jealous ex-husband, it proves that 'no one delivers an emotional read like Colleen Hoover' (Anna Todd, bestselling author).",
+      "id": 2
+    },
+    {
+      "src": "img/Alan.jpg",
+      "alt": "Book",
+      "author": "Alan Rickman",
+      "bookName": "Madly, Deeply: The Alan Rickman Diaries",
+      "price": 25,
+      "descr": "Alan Rickman remains one of the most beloved actors of all time across almost every genre, from his breakout role as Die Hard's villainous Hans Gruber to his heart-wrenching run as Professor Severus Snape, and beyond. His air of dignity, his sonorous voice and the knowing wit he brought to each role continue to captivate new audiences today.But Rickman's artistry wasn't confined to just his performances. Fans of memoirs at large will delight in the intimate experience of reading Rickman detailing the extraordinary and the ordinary in a way that is 'anecdotal, indiscreet, witty, gossipy and utterly candid'. He grants us access to his thoughts, not only on plays, films and the craft of acting, but also politics, friendships and life.The Rickman Diaries was written with the intention to be shared, and reading it is like listening to Rickman chatting to a close friend.",
+      "id": 3
+    },
+    {
+      "src": "img/Heartstopper.jpg",
+      "alt": "Book",
+      "author": "Alice Oseman",
+      "bookName": "The Heartstopper Yearbook",
+      "price": "16",
+      "descr": "Soon to be a live-action Netflix series! Boy meets boy. Boys become friends. Boys fall in love.This joyful trip into the LGBTQ+ world of Heartstopper is the perfect gift for anyone who loves the graphic novels or Netflix TV series - from Alice Oseman, bestselling author and winner of the YA Book Prize.Now in full colour for the first time!The full-colour Heartstopper Yearbook is packed full of exclusive content from the Heartstopper universe - including never-before-seen illustrations, an exclusive mini-comic, a look back at Alice's Heartstopper artwork over the years, character profiles, trivia, and insights into her creative process - all narrated by a cartoon version of Alice herself.By the winner of the YA Book Prize, Heartstopper is about love, friendship, loyalty and mental illness. It encompasses all the small stories of Nick and Charlie's lives that together make up something larger, which speaks to all of us",
+      "id": 4
+    },
+    {
+      "src": "img/Felton.jpg",
+      "alt": "Book",
+      "author": "Tom Felton",
+      "bookName": "Beyond the Wand",
+      "price": "16.99",
+      "descr": "They called for a break, and Gambon magicked up a cigarette from out of his beard. He and I were often to be found outside the stage door, having 'a breath of fresh air', as we referred to it. There would be painters and plasterers and chippies and sparks, and among them all would be me and Dumbledore having a crafty cigarette.From Borrower to wizard, Tom Felton's adolescence was anything but ordinary. His early rise to fame saw him catapulted into the limelight aged just twelve when he landed the iconic role of Draco Malfoy in the Harry Potter films.Speaking with candour and his own trademark humour, Tom shares his experience of growing up on screen and as part of the wizarding world for the very first time. He tells all about his big break, what filming was really like and the lasting friendships he made during ten years as part of the franchise, as well as the highs and lows of fame and the reality of navigating adult life after filming finished.",
+      "id": 5
+    },
+    {
+      "src": "img/Moons.jpg",
+      "alt": "Book",
+      "author": "Shehan Karunatilaka",
+      "bookName": "The Seven Moons of Maali Almeida",
+      "price": "16.99",
+      "descr": "A searing satire set amid the murderous mayhem of Sri Lanka beset by civil war Colombo, 1990.Maali Almeida, war photographer, gambler and closet gay, has woken up dead in what seems like a celestial visa office. His dismembered body is sinking in the serene Beira lake and he has no idea who killed him.At a time where scores are settled by death squads, suicide bombers and hired goons, the list of suspects is depressingly long, as the ghouls and ghosts with grudges who cluster round can attest. But even in the afterlife, time is running out for Maali. He has seven moons to try and contact the man and woman he loves most and lead them to a hidden cache of photos that will rock Sri Lanka.Ten years after his prizewinning novel Chinaman established him as one of Sri Lanka's foremost authors, Karunatilaka is back with a rip-roaring epic, full of mordant wit and disturbing truths.",
+      "id": 6
+    },
+    {
+      "src": "img/Glad.jpg",
+      "alt": "Book",
+      "author": "Jennette McCurdy",
+      "bookName": "I'm Glad My Mom Died",
+      "price": "17.99",
+      "descr": "A heartbreaking and hilarious memoir by iCarly and Sam & Cat star Jennette McCurdy about her struggles as a former child actor-including eating disorders, addiction, and a complicated relationship with her overbearing mother-and how she retook control of her life.Jennette McCurdy was six years old when she had her first acting audition. Her mother's dream was for her only daughter to become a star, and Jennette would do anything to make her mother happy. So she went along with what Mom called Calorie restriction, eating little and weighing herself five times a day. She endured extensive at-home makeovers while Mom chided. Your eyelashes are invisible, okay? You think Dakota Fanning doesn't tint hers?' She was even showered by Mom until age sixteen while sharing her diaries, email, and all her income.",
+      "id": 7
+    },
+    {
+      "src": "img/Climate.jpg",
+      "alt": "Book",
+      "author": "Greta Thunberg",
+      "bookName": "The Climate Book",
+      "price": "21.99",
+      "descr": "Greta Thunberg's speeches shook the world. With The Climate Book, she has created an essential tool for everyone who wants to help save it.It seems like an impossible task: secure a safe future for life on Earth, acting at a scale and speed that the world has never seen, in the face of vast and powerful forces--not just oil tycoons and governments, but the changing climate system itself. The odds are against us, and we are running out of time. But it doesn't have to be this way.Around the world, geophysicists and mathematicians, oceanographers and meteorologists, engineers, economists, psychologists and philosophers have been using their expertise to develop a deep understanding of the crises we face. Greta Thunberg has created The Climate Book in partnership with over one hundred of these experts in order to equip us all with this knowledge. Alongside them, Greta shares her own stories of learning, demonstrating, and uncovering greenwashing around the world, revealing the extent to which we have been kept in the dark. This is one of our biggest problems, she shows, but also our greatest source of hope. Once we are given the full picture, we will be able to act--and if a schoolchild's strike could ignite a global protest, what could we do collectively if we tried?",
+      "id": 8
+    }
+  ]
 };
 
 const getResource = () => {
-    return books.booksDB;
+  return books.booksDB;
 };
 
 
